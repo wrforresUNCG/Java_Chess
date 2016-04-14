b57aa2a469e940508d953044d6881f49c7a7866d
rfernanuncg <r_fernan@uncg.edu>
2016-04-14 12:56:12 -0400

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,32 @@
 
 public class Board {
-
+Space[][] chessBoard = new Space[8][8];
+    
+    public Board(){
+        
+        for(int i=0;i<chessBoard.length;i++){
+            for(int j=0;j<chessBoard.length;j++){
+                chessBoard[i][j]= new Space(i,j);
+            }
+        }
+        
+    }
+    
+    public Space getSpace(int i, int j){
+            return chessBoard[i][j];
+        }
+    
+    public void printBoard(){
+          int count =0;
+          for(int i=0;i<chessBoard.length;i++){
+            for(int j=0;j<chessBoard.length;j++){
+                System.out.printf(chessBoard[i][j].getSymbol());
+                count++;
+                if(count == 8){
+                    System.out.print("\n");
+                    count =0;
+                }
+            }
+        }
+    }
 }
