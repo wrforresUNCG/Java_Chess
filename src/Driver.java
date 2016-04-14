1a2efd2870042370773d087b399a12f4d24b70f9
loganrohde <logan.rohde@gmail.com>
2016-04-14 13:22:51 -0400

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,17 @@
 
 public class Driver {
-
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean white = true; //white pieces are capital case
+        boolean black = false; //black pieces are lowercase
+        boolean win = false;
+        
+        Board b = new Board();
+        while(win == false){
+            
+        }
+           
+    }
 }
