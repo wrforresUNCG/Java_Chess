095a3e782dc98c21b1af5e47f7c22f1ad58991e9
loganrohde <logan.rohde@gmail.com>
2016-04-14 13:15:03 -0400

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -12,6 +12,133 @@ Space[][] chessBoard = new Space[8][8];
         
     }
     
+    public void convert(String pos){
+        Scanner in = new Scanner(pos);
+        in.useDelimiter(":");
+        String temp;
+        int count = 0;
+        
+        parse:
+        while(in.hasNext()){
+            if(count == 0){
+                startPos = in.next();
+                count++;
+            }
+            else if(count == 1){
+                endPos = in.next();
+                count++;
+            }
+
+        }
+        
+        //Convert start position to array values
+        for(int i = 1; i < 9; i++){
+            if(startPos.compareTo("a" + i) == 0){
+                startX = 8 - i;
+                startY = 0;
+            }
+        }
+        for(int i = 1; i < 9; i++){
+            if(startPos.compareTo("b" + i) == 0){
+                startX = 8 - i;
+                startY = 1;
+            }
+        }
+        for(int i = 1; i < 9; i++){
+            if(startPos.compareTo("c" + i) == 0){
+                startX = 8 - i;
+                startY = 2;
+            }
+        }
+        for(int i = 1; i < 9; i++){
+            if(startPos.compareTo("d" + i) == 0){
+                startX = 8 - i;
+                startY = 3;
+            }
+        }
+        for(int i = 1; i < 9; i++){
+            if(startPos.compareTo("e" + i) == 0){
+                startX = i - 1;
+                startY = 4;
+            }
+        }
+        for(int i = 1; i < 9; i++){
+            if(startPos.compareTo("f" + i) == 0){
+                startX = 8 - i;
+                startY = 5;
+            }
+        }
+        for(int i = 1; i < 9; i++){
+            if(startPos.compareTo("g" + i) == 0){
+                startX = 8 - i;
+                startY = 6;
+            }
+        }
+        for(int i = 1; i < 9; i++){
+            if(startPos.compareTo("h" + i) == 0){
+                startX = 8 - i;
+                startY = 7;
+            }
+        }
+        
+        
+        //convert end position to array values
+        for(int i = 1; i < 9; i++){
+            if(endPos.compareTo("a" + i) == 0){
+                endX = 8 - i;
+                endY = 0;
+            }
+        }
+        for(int i = 1; i < 9; i++){
+            if(endPos.compareTo("b" + i) == 0){
+                endX = 8 - i;
+                endY = 1;
+            }
+        }
+        for(int i = 1; i < 9; i++){
+            if(endPos.compareTo("c" + i) == 0){
+                endX = 8 - i;
+                endY = 2;
+            }
+        }
+        for(int i = 1; i < 9; i++){
+            if(endPos.compareTo("d" + i) == 0){
+                endX = 8 - i;
+                endY = 3;
+            }
+        }
+        for(int i = 1; i < 9; i++){
+            if(endPos.compareTo("e" + i) == 0){
+                endX = 8 - i;
+                endY = 4;
+            }
+        }
+        for(int i = 1; i < 9; i++){
+            if(endPos.compareTo("f" + i) == 0){
+                endX = 8 - i;
+                endY = 5;
+            }
+        }
+        for(int i = 1; i < 9; i++){
+            if(endPos.compareTo("g" + i) == 0){
+                endX = 8 - i;
+                endY = 6;
+            }
+        }
+        for(int i = 1; i < 9; i++){
+            if(endPos.compareTo("h" + i) == 0){
+                endX = 8 - i;
+                endY = 7;
+            }
+        }
+        
+         
+        
+        
+         System.out.println("The start position is: " + startX + ", " + startY);
+         System.out.println("The end position is: " + endX + ", " + endY);
+    }
+    
     public Space getSpace(int i, int j){
             return chessBoard[i][j];
         }
