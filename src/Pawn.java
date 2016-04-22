ab10f156a74d9c88913e5a0ea8943bde19646985
loganrohde <logan.rohde@gmail.com>
2016-04-22 12:11:38 -0400

    FirstMove update
    Now allows the pawn to move two spaces if it is their first move.

--- src/Pawn.java
+++ src/Pawn.java
@@ -2,7 +2,7 @@
  * Class representing a Pawn chess piece.
  * 
  * @author William Forrest
- *
+ * @author Logan Rohde
  */
 public class Pawn extends Piece{
 
@@ -12,6 +12,7 @@ public class Pawn extends Piece{
 	private String curPos;
 	private int curX;
 	private int curY;
+	private boolean firstMove = true;
 	
 	public Pawn(){
 		String type = "Pawn";
@@ -34,39 +35,65 @@ public class Pawn extends Piece{
 	@Override
 	public boolean validMove(Space[][] chessBoard, int staX, int staY, int tarX, int tarY) {
 		// Determine color of piece to be moved
-		String teamOcc = chessBoard[staY][staX].piece.getColor();
-		
-		// Calculate Valid Move Conditions
-		boolean valMov = false;
-		
-		// A White Pawn moves up 1, row index decreasing
-		if(teamOcc.equals("white") && (tarY - staY) == -1){
-			if(tarX == staX){
-				valMov = valUp(chessBoard, staX, staY, tarX, tarY, teamOcc);
-			}
-			else if(tarX == (staX - 1)){
-				valMov = valUpLft(chessBoard, staX, staY, tarX, tarY, teamOcc);
-			}
-			else if(tarX == (staX + 1)){
-				valMov = valUpRt(chessBoard, staX, staY, tarX, tarY, teamOcc);
-			}
-			else return false;
-		}
-		//A Black Pawn moves down 1, row index increasing
-		else if(teamOcc.equals("black") && (tarY - staY == 1)){
-			if(tarX == staX){
-				valMov = valDwn(chessBoard, staX, staY, tarX, tarY, teamOcc);
-			}
-			else if(tarX == (staX - 1)){
-				valMov = valDwnLft(chessBoard, staX, staY, tarX, tarY, teamOcc);
-			}
-			else if(tarX == (staX + 1)){
-				valMov = valDwnRt(chessBoard, staX, staY, tarX, tarY, teamOcc);
-			}
-			else return false;
-		}
-		
-		return valMov;
+        	String teamOcc = chessBoard[staY][staX].piece.getColor();
+
+        	// Calculate Valid Move Conditions
+        	boolean valMov = false;
+	 	//Allow the pawn to move two places if it is its first move
+        	if (firstMove) {
+
+            		if (teamOcc.equals("white") && (((tarY - staY) == -1) || (tarY - staY == -2))) {
+                		if (tarX == staX) {
+                    			valMov = valUp(chessBoard, staX, staY, tarX, tarY, teamOcc);
+                		} else if (tarX == (staX - 1)) {
+                    			valMov = valUpLft(chessBoard, staX, staY, tarX, tarY, teamOcc);
+                		} else if (tarX == (staX + 1)) {
+                    			valMov = valUpRt(chessBoard, staX, staY, tarX, tarY, teamOcc);
+                		} else {
+                    			return false;
+                		}
+            		} //A Black Pawn moves down 1, row index increasing
+            		else if (teamOcc.equals("black") && (((tarY - staY) == 1) || (tarY - staY == 2))) {
+                		if (tarX == staX) {
+                    			valMov = valDwn(chessBoard, staX, staY, tarX, tarY, teamOcc);
+                		} else if (tarX == (staX - 1)) {
+                    			valMov = valDwnLft(chessBoard, staX, staY, tarX, tarY, teamOcc);
+                		} else if (tarX == (staX + 1)) {
+                    			valMov = valDwnRt(chessBoard, staX, staY, tarX, tarY, teamOcc);
+                		} else {
+                    			return false;
+                		}
+            		}
+            		firstMove = false;
+        	} else {
+            		// A White Pawn moves up 1, row index decreasing
+            		if (teamOcc.equals("white") && (tarY - staY) == -1) {
+                		if (tarX == staX) {
+                    			valMov = valUp(chessBoard, staX, staY, tarX, tarY, teamOcc);
+                		} else if (tarX == (staX - 1)) {
+                			valMov = valUpLft(chessBoard, staX, staY, tarX, tarY, teamOcc);
+                		} else if (tarX == (staX + 1)) {
+                    			valMov = valUpRt(chessBoard, staX, staY, tarX, tarY, teamOcc);
+                		} else {
+                    			return false;
+                		}
+            		} //A Black Pawn moves down 1, row index increasing
+            		else if (teamOcc.equals("black") && (tarY - staY == 1)) {
+                		if (tarX == staX) {
+                    			valMov = valDwn(chessBoard, staX, staY, tarX, tarY, teamOcc);
+        			} else if (tarX == (staX - 1)) {
+                    			valMov = valDwnLft(chessBoard, staX, staY, tarX, tarY, teamOcc);
+                		} else if (tarX == (staX + 1)) {
+                    			valMov = valDwnRt(chessBoard, staX, staY, tarX, tarY, teamOcc);
+                		} else {
+                    			return false;
+                		}
+            		}
+            		firstMove = false;
+        	}
+        	firstMove = false;
+
+        	return valMov;
 	}
 	
 	public boolean valUp(Space[][] chessBoard, int staX, int staY, int tarX, int tarY, String teamOcc){
